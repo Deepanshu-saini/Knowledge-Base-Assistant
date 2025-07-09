607e788892d9992717cc9196eb7fbabf319785df
Deepanshu saini <saindeepansh@gmail.com>
2025-07-09 21:48:15 +0530

    model and endpoint for document

--- knowledge_assistant/models.py
+++ knowledge_assistant/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Document(models.Model):
+    file = models.FileField(upload_to='documents/')
+    name = models.CharField(max_length=255)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+class Chunk(models.Model):
+    document = models.ForeignKey(Document, related_name='chunks', on_delete=models.CASCADE)
+    text = models.TextField()
+    embedding = models.BinaryField()  # Store as bytes; can be changed to ArrayField if using Postgres
+    page_number = models.IntegerField(null=True, blank=True)
+    order = models.IntegerField(default=0)  # Order of chunk in document
+
+    def __str__(self):
+        return f"Chunk {self.order} of {self.document.name}"
--- knowledge_assistant/views.py
+++ knowledge_assistant/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.parsers import MultiPartParser, FormParser
+from rest_framework.response import Response
+from .models import Document
+from .serializers import DocumentSerializer
 
 # Create your views here.
+
+class DocumentViewSet(viewsets.ModelViewSet):
+    queryset = Document.objects.all()
+    serializer_class = DocumentSerializer
+    parser_classes = (MultiPartParser, FormParser)
